5d92da8792135ab4fb0815e8e580188ae7c81ead
Arthur Johnston <amjohnst@usc.edu>
2017-12-01 12:50:07 -0800

    Basic data visualization

--- data_vis.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ajohnston
- * Date: 11/28/17
- * Time: 3:55 PM
- */
--- datavis.php
+++ datavis.php
@@ -1,7 +1,63 @@
+<html>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: ajohnston
- * Date: 12/1/17
- * Time: 11:32 AM
- */
+session_start();
+$mysql = new mysqli(
+    "acad.itpwebdev.com",
+    halpan,
+    Pleasejustletmein4726,
+    "halpan_reflora");
+
+if($mysql->connect_errno) {
+    echo "db connection error : " . $mysql->connect_error;
+    exit();
+}
+
+//find average red value
+$sql = "SELECT AVG(red) as average_red FROM entries";
+
+$results = $mysql->query($sql);
+
+$row = mysqli_fetch_assoc($results);
+
+$red = floor($row['average_red']);
+
+//find average green value
+$sql = "SELECT AVG(green) as average_green FROM entries";
+
+$results = $mysql->query($sql);
+
+$row = mysqli_fetch_assoc($results);
+
+$green = floor($row['average_green']);
+
+//find average blue value
+$sql = "SELECT AVG(blue) as average_blue FROM entries";
+
+$results = $mysql->query($sql);
+
+$row = mysqli_fetch_assoc($results);
+
+$blue = floor($row['average_blue']);
+?>
+<head>
+  <title>Color Data Visualization</title>
+    <link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
+<body>
+<div class="colorbar" style="background-color:rgb(<?php echo $red . "," . $green . "," .$blue;?>);"><h1>Average color</h1></div>
+<div class="colorbar"><div class="colorbarfill" style="background-color:red;width: <?php echo $red;?>px;"><h1>Average red value</h1></div></div><br>
+<div class="colorbar"><div class="colorbarfill" style="background-color:green;width: <?php echo $green;?>px;"><h1>Average green value</h1></div></div><br>
+<div class="colorbar"><div class="colorbarfill" style="background-color:blue;width: <?php echo $blue;?>px;"><h1>Average blue value</h1></div></div><br>
+
+
+
+</body>
+
+
+</html>
+
+
+
+<?php
+session_start();
