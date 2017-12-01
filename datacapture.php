95ef96cfb2eeff3e126d20a3b5e128d07b1d89a2
halpan2001 <halpan@usc.edu>
2017-11-30 23:54:56 -0800

    beginning admin entries and the data capture functionality

--- datacapture.php
+++ datacapture.php
@@ -9,3 +9,42 @@
 /* some Javascript that once the capture button is clicked we can grab and pass through the settings data
 
 /* secondary form that is made up of a lot of hidden things with values and when hit capture it also submits the form?
+
+*/
+
+$rvalue = "rSlider.value";
+$gvalue = "gSlider.value";
+$bvalue = "bSlider.value";
+$avalue = "aSlider.value";
+$sizevalue = "sizeSlider.value";
+$speedvalue = "speedSlider.value";
+$authorvalue = "authorSlider.value";
+
+$mysql = new mysqli(
+    "acad.itpwebdev.com",
+    halpan,
+    Pleasejustletmein4726,
+    "halpan_reflora");
+
+if($mysql->connect_errno) {
+    echo "db connection error : " . $mysql->connect_error;
+    exit();
+}
+
+$sql = "INSERT INTO entries (email, username, password, date_join) VALUES ('".
+    $_REQUEST['email']. "','".
+    $_REQUEST['name']. "','".
+    $_REQUEST['password1']. "',' ".
+    $date.
+    "')";
+
+$results = $mysql->query($sql);
+
+if(!$results) {
+    echo "SQL error: ". $mysql->error;
+    exit();
+} else {
+    $_SESSION["newaccount"]="yes";
+    $_SESSION["loggedin"]="yes";
+    include "index.php";
+}
