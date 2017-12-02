9f64c15a7f11950e21493129eec97450e2d56618
Arthur Johnston <amjohnst@usc.edu>
2017-12-02 14:03:10 -0800

    upload link

--- index.php
+++ index.php
@@ -353,6 +353,7 @@
         $start = 1;
         $end = $start+2;
         $counter = $start;
+        $result->data_seek($start-1);
         while ($row = mysqli_fetch_array($result)) {
             echo "<img src='http://acad.itpwebdev.com/~amjohnst/ScheduleSearch/reflora/images/reflora.jpeg' >";
             if($end <= $counter) {
